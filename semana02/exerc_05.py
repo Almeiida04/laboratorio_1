a8828f23f6541d0bf8a92b6710bf513410a63a6b
Gabriel Almeida <gabriel.almeida@amf.edu.br>
2024-08-25 13:48:41 -0300

    Create exerc_05.py

---
+++ semana02/exerc_05.py
@@ -0,0 +1,10 @@
+pneus = 395.40
+rodas = 4
+
+valor1 = pneus * rodas
+ 
+carrosclientes = float(input("Insira o número de carros que deseja fazer a troca de todos os pneus:"))
+
+valortotal = carrosclientes * valor1
+
+print("Valor total de:", valortotal)
