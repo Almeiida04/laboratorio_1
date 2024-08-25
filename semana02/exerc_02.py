a74f16c07e52396d63f1e30e4653ec050ec9ff5b
Gabriel Almeida <gabriel.almeida@amf.edu.br>
2024-08-25 13:44:54 -0300

    Create exerc_02.py

---
+++ semana02/exerc_02.py
@@ -0,0 +1,7 @@
+precofabrica = float(input("Insira o valor da peça de roupa:"))
+acrescimo = 0.30
+
+preco_final = precofabrica * (1 + acrescimo)
+
+print("O valor final é de:{}".format(preco_final))
+
