ec6f0d0e9613d835bcd9c1676eba7d4891baa717
Gabriel Almeida <gabriel.almeida@amf.edu.br>
2024-11-12 12:46:08 -0300

    .

---
+++ semana12.py/ex01.py
@@ -0,0 +1,24 @@
+def numeros_inteiros():
+    numeros = []
+
+    for i in range(8):
+        num = float(input("Digite um número: "))
+        numeros.append(num)
+
+    return numeros
+
+def maior_numeros(numeros):
+    for i in range(len(numeros)):
+        if len(numeros) > 30:
+            len(numeros)
+            print(f"Os números maiores que 30 são: {len(numeros)}")
+
+def soma(numeros):
+    somar = sum(numeros)
+    print(f"A soma dos números é {numeros} é {soma}")
+
+def soma_todos(numeros):
+    somar = 0
+
+def main():
+    numeros = 
